320e403af85f6d41bd4e0a707e6272c1be9590a2
Woodpecker CI <ci@woodpecker-ci.org>
2025-11-11 10:53:11 +0000

    Update TumblePipe framework
    Published from pipeline repository commit 4d12bc1b
    
    This release includes:
    - Core Houdini HDAs and Python framework
    - Deadline render farm plugins
    - OCIO color configuration
    - Example configurations
    
    Studio-specific configurations excluded.

---
+++ houdini/TumblePipe/python3.11libs/pythonrc.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import platform
+import sys
+import os
+
+def load():
+
+    # Paths
+    pipeline_path = Path(os.environ['TH_PIPELINE_PATH'])
+    package_path = pipeline_path / 'houdini' / 'Tumblehead'
+    libs_path = package_path / 'python3.11libs'
+
+    # Add the shared python packages location to sys.path
+    packages_path = package_path / 'python' / '1x'
+    sys.path.insert(0, str(packages_path))
+
+    # Add the additional python packages location to sys.path
+    external_path = (
+        libs_path /
+        'external' /
+        platform.system().lower() /
+        'Lib' /
+        'site-packages'
+    )
+    sys.path.insert(0, str(external_path))
+
+load()
