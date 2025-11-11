34ce63ccffc1851080939d03f55a63b6db25faa4
Soren Norbaek <sorennorbaek@gmail.com>
2025-11-11 12:46:42 +0100

    Restore examples directory
    The examples directory was accidentally deleted by the automated
    publish workflow. Restored from commit 79baf9d.
    
    The publish script has been updated to exclude examples/ from sync,
    as it is unique to TumblePipe and should be managed separately.

---
+++ examples/config/storage_convention.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from tumblehead.storage import StorageConvention
+from tumblehead.api import (
+    get_config_path,
+    get_project_path,
+    get_pipeline_path,
+    get_project_name
+)
+
+class _StorageConvention(StorageConvention):
+    def __init__(self):
+        _home = Path('/mnt/c/users/tumblehead')
+        project_name = get_project_name()
+        self.edit_path = Path('/mnt/z/') / project_name
+        self.temp_path = _home / 'th_temp' / project_name
+        self.proxy_path = _home / 'th_proxy' / project_name
+        self.project_path = get_project_path()
+        self.pipeline_path = get_pipeline_path()
+        self.config_path = get_config_path()
+        self.assets_path = self.project_path / 'assets'
+        self.shots_path = self.project_path / 'shots'
+        self.kits_path = self.project_path / 'kits'
+        self.export_path = self.project_path / 'export'
+        self.cache_path = self.project_path.parent / f'{project_name}_cache'
+        self.render_path = self.project_path.parent / f'{project_name}_render'
+        self.turntable_path = self.project_path.parent / f'{project_name}_turntable'
+        self.preset_path = self.config_path / 'presets'
+
+    def resolve(self, path):
+        if not self.is_valid_path(path): return None
+        purpose, parts = self.parse_path(path)
+        match purpose:
+            case 'edit': return self.edit_path / Path(*parts)
+            case 'temp': return self.temp_path / Path(*parts)
+            case 'proxy': return self.proxy_path / Path(*parts)
+            case 'project': return self.project_path / Path(*parts)
+            case 'pipeline': return self.pipeline_path / Path(*parts)
+            case 'config': return self.config_path / Path(*parts)
+            case 'assets': return self.assets_path / Path(*parts)
+            case 'shots': return self.shots_path / Path(*parts)
+            case 'kits': return self.kits_path / Path(*parts)
+            case 'export': return self.export_path / Path(*parts)
+            case 'cache': return self.cache_path / Path(*parts)
+            case 'render': return self.render_path / Path(*parts)
+            case 'turntable': return self.turntable_path / Path(*parts) 
+            case 'preset': return self.preset_path / Path(*parts)
+            case _: assert False, f'Unknown path purpose "{purpose}"'
+
+def create():
+    return _StorageConvention()
